41e9d34a8508fd4336ed64f8a85a1f776b406a9d
saudsaad2015 <saudsaad.2015@gmail.com>
2024-10-29 02:01:53 +0300

    تحديث الكود وإصلاح الأخطاء

--- scripts/pi-hole/php/theme.php
+++ scripts/pi-hole/php/theme.php
@@ -43,7 +43,7 @@ if (isset($_POST['field'])
 if (!array_key_exists($webtheme, $available_themes)) {
     // Fallback to default (light) theme is property is not set
     // or requested theme is not among the available
-    $webtheme = 'default-auto';
+    $webtheme = 'light-theme';
 }
 
 $darkmode = $available_themes[$webtheme][1];
